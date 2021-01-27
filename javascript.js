8bbd0d9dacaea50821703894b81835ac3c722cc7
Reema Eilouti <reema.eilouti@gmail.com>
2021-01-27 16:18:20 +0200

    final touch 3

--- javascript.js
+++ javascript.js
@@ -2,8 +2,6 @@ var firstname = prompt("Hey! What's your name?");
 
 var birthYear = prompt("What year were you born in?");
 
-var age = (2021 - birthYear)
-
 
 
 if (confirm("Press a button!")) 
@@ -19,6 +17,8 @@ else
 
 var element1 = document.getElementById("greeting");
 element1.textContent = "Welcome " + firstname + "!";
+
+var age = 2021 - birthYear
 
 var element2 = document.getElementById("years");
 element2.textContent = "Happy " + age + " Years!";
