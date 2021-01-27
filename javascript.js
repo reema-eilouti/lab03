673b8ae0cc75bea8e75d96afa02d75ce089a2d9b
Reema Eilouti <reema.eilouti@gmail.com>
2021-01-27 15:38:29 +0200

    adding a confirm box

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var name = prompt("Hey! What's your name?");
 
-document.getElementById("greeting").innerText("Welcomes ");
+// document.getElementById("greeting").innerText("Welcomes ");
 // document.write("Welcome " );
 
 if (confirm("Press a button!")) 
