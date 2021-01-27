e3f62df8416bc31ee9294d7de1facf18a706d9c7
Reema Eilouti <reema.eilouti@gmail.com>
2021-01-27 15:37:09 +0200

    moving on to another task

--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@ var name = prompt("Hey! What's your name?");
 
 document.getElementById("greeting").innerText("Welcomes ");
 // document.write("Welcome " );
+
+if (confirm("Press a button!")) 
+{
+    txt = "You pressed OK!";
+} 
+else 
+{
+    txt = "You pressed Cancel!";
+}
