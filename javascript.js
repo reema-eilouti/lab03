524d73fc7aa1a199857b7a76134f4625b6e68dc6
Reema Eilouti <reema.eilouti@gmail.com>
2021-01-27 15:27:11 +0200

    fixing bugs

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 var name = prompt("Hey! What's your name?");
 
-// document.getElementById("greeting").innerHTML("Welcome " + name + "!");
-document.write("Welcome " + name + "!");
+document.getElementById("greeting").innerHTML("Welcomes ");
+document.write("Welcome " );
