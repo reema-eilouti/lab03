c8eff6b89b04309321e1b9abefaf3fedf0d6ff0b
Reema Eilouti <reema.eilouti@gmail.com>
2021-01-27 15:23:18 +0200

    testing errors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
 var name = prompt("Hey! What's your name?");
 
-document.getElementById("greeting").innerHTML("Welcome " + name + "!");
+// document.getElementById("greeting").innerHTML("Welcome " + name + "!");
+document.write("Welcome " + name + "!");
